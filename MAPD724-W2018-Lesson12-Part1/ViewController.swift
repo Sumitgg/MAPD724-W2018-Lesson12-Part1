19845e215f1910017e75f276173a58d29979cb1b
Sumit Ganju <sganju@Sumits-MacBook-Pro.local>
2018-04-19 11:39:22 -0400

    Added MapKitView to Storyboard - closes #1

--- MAPD724-W2018-Lesson12-Part1/ViewController.swift
+++ MAPD724-W2018-Lesson12-Part1/ViewController.swift
@@ -1,23 +1,12 @@
-//
-//  ViewController.swift
-//  MAPD724-W2018-Lesson12-Part1
-//
-//  Created by Sumit Ganju on 2018-04-19.
-//  Copyright © 2018 Centennial College. All rights reserved.
-//
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
 
